ed287d29974387c883e9f098c1d6cabb8d196f52
jcm321 <jcm321@lehigh.edu>
2021-08-04 17:23:51 +0000

    7/15 10:08  Test

--- shell_test.py
+++ shell_test.py
@@ -1,4 +1,8 @@
 import os
 os.system("git init")
 os.system("git commit -am '7/15 10:08  Test' ")
+username = "jcm321"
+pswd = "Iwillgetin9!"
 os.system("git push")
+os.system(username) 
+os.system(pswd)
